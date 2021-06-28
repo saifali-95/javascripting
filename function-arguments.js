2244f36fb3878677cde2d1b6adaefe869864c804
Saif Ali <saifali7646@gmail.com>
2021-06-28 03:23:03 +0000

    Initial commit with all my work so far

---
+++ function-arguments.js
@@ -0,0 +1,5 @@
+function math(a,b,c){
+  return sum = a+(b*c);
+}
+
+console.log(math(53,61,67));
---
+++ revising-strings.js
@@ -0,0 +1,3 @@
+let pizza =  'pizza is alright';
+change = pizza.replace('alright', 'wonderful');
+console.log(change);
